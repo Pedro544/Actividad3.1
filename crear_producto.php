0c0919977b6e486611e4075f8b64b47a71f10f69
PedroElRincón <pedrogarciasantana@alumno.ieselrincon.es>
2023-11-04 16:50:24 +0100

    Creada estructura condicional para envío de datos

--- crear_producto.php
+++ crear_producto.php
@@ -1,30 +1,47 @@
+<?php if (empty($_POST)): ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="author" content="Pedro García Santana">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Creación de producto</title>
-        <link rel="stylesheet" href="styles/styles.css">
-    </head>
-    <body>
-        <h1>Nuevo producto</h1>
-        <h2>Ingresa la información del nuevo producto</h2>
-        <form action="crear_producto.php" method="post" enctype="multipart/form-data">
-            <label for="nombre">Nombre del producto</label>
-            <input type="text" name="nombre" id="nombre">
-            <label for="precio">Precio del producto</label>
-            <input type="number" name="precio" id="precio">
-            <label for="imagen">Imagen del producto</label>
-            <input type="file" name="imagen" id="imagen">
-            <label for="categoria">Categoría del producto</label>
-            <select name="categoria" id="categoria">
-                <option value="cat1">Categoria1</option>
-                <option value="cat2">Categoria2</option>
-                <option value="cat3">Categoria3</option>
-                <option value="cat4">Categoria4</option>
-            </select>
-            <button type="submit">Insertar producto</button>
-        </form>
-    </body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Pedro García Santana">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creación de producto (Creación)</title>
+    <link rel="stylesheet" href="styles/styles.css">
+  </head>
+  <body>
+    <h1>Nuevo producto</h1>
+    <h2>Ingresa la información del nuevo producto</h2>
+    <form action="crear_producto.php" method="post" enctype="multipart/form-data">
+      <label for="nombre">Nombre del producto</label>
+      <input type="text" name="nombre" id="nombre">
+      <label for="precio">Precio del producto</label>
+      <input type="number" name="precio" step="0.01" id="precio">
+      <label for="imagen">Imagen del producto</label>
+      <input type="file" name="imagen" id="imagen">
+      <label for="categoria">Categoría del producto</label>
+      <select name="categoria" id="categoria">
+        <option value="cat1">Categoria1</option>
+        <option value="cat2">Categoria2</option>
+        <option value="cat3">Categoria3</option>
+        <option value="cat4">Categoria4</option>
+      </select>
+      <button type="submit">Insertar producto</button>
+    </form>
+  </body>
 </html>
+<?php else :?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Pedro García Santana">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creación de producto (Resultado)</title>
+    <link rel="stylesheet" href="styles/styles.css">
+  </head>
+  <body>
+    <h1>Esto tendrá la validación y tal</h1>
+    <a href="crear_producto.php"><button type="button">Volver</button></a>
+  </body>
+</html>
+<?php endif; ?>
