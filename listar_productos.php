6e216b4c26ab7c58f58b644639121fd4abb0b620
PedroElRincón <pedrogarciasantana@alumno.ieselrincon.es>
2023-11-06 12:15:41 +0100

    Creados estilos de la tabla

---
+++ listar_productos.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Pedro García Santana">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de productos</title>
+    <link rel="stylesheet" href="styles/styles.css">
+  </head>
+  <body>
+    <table>
+      <caption>Listado de productos</caption>
+      <tr>
+        <th>Nombre</th>
+        <th>Precio</th>
+        <th>Imagen</th>
+        <th>Categoría</th>
+        <th>Modificar</th>
+        <th>Eliminar</th>
+      </tr>
+      <tr>
+        <td>Nombre</td>
+        <td>Precio</td>
+        <td><img src="imgs/Euler.png" alt="euler" width="100" height="100"></td>
+        <td>Categoría</td>
+        <td><img src="imgs/icons8-modify-58.png" alt="modify"></td>
+        <td><img src="imgs/icons8-delete-100.png" alt="delete"></td>
+      </tr>
+      <tr>
+        <td>Nombre</td>
+        <td>Precio</td>
+        <td><img src="imgs/Euler.png" alt="euler" width="100" height="100"></td>
+        <td>Categoría</td>
+        <td><img src="imgs/icons8-modify-58.png" alt="modify"></td>
+        <td><img src="imgs/icons8-delete-100.png" alt="delete"></td>
+      </tr>
+      <tr>
+        <td>Nombre</td>
+        <td>Precio</td>
+        <td><img src="imgs/Euler.png" alt="euler" width="100" height="100"></td>
+        <td>Categoría</td>
+        <td><img src="imgs/icons8-modify-58.png" alt="modify"></td>
+        <td><img src="imgs/icons8-delete-100.png" alt="delete"></td>
+      </tr>                
+    </table>
+  </body>
+</html>
