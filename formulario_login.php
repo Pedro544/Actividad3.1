749a2fb921534b5b1031176f2cf6653e847edf04
PedroElRincón <pedrogarciasantana@alumno.ieselrincon.es>
2023-11-18 19:46:07 +0100

    Implementada validación de formulario

--- formulario_login.php
+++ formulario_login.php
@@ -8,13 +8,35 @@
     <link rel="stylesheet" href="styles/styles.css">
   </head>
   <body>
+    <?php if (empty($_POST)):?>
     <h1>Autenticación para entrar a la aplicación de productos</h1>
     <form action="formulario_login.php" method="post">
       <label for="correo">Correo electrónico</label>
-      <input type="email" name="correo" id="correo">
+      <input type="email" name="correo" id="correo" required>
       <label for="contraseña">Contraseña</label>
-      <input type="password" name="contraseña" id="contraseña">
+      <input type="password" name="contraseña" id="contraseña" required>
       <button type="submit">Enviar</button>
     </form>
+    <?php else:?>
+    <?php
+    include "conexion.php";
+    $consulta_ejecutada = $conexion->query(
+        "SELECT 
+        id,
+        correo_electronico,
+        contrasena_hash
+        FROM usuarios
+        WHERE correo_electronico='{$_POST["correo"]}'"
+    );
+    $datos = $consulta_ejecutada->fetch(PDO::FETCH_ASSOC);
+    if (!$datos || !password_verify($_POST["contraseña"],$datos["contrasena_hash"])){
+        echo "<h2>ERROR: El correo electrónico o contraseña no es correcto.</h2>";
+    } else{
+        session_start();
+        $_SESSION["ID_USUARIO"] = $datos["id"];
+        header("Location: index.php");
+    }
+    ?>
+    <?php endif;?>
   </body>
 </html>
